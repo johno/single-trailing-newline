f22f8f9c0b4039ed6e62dda967dda7e10490211a
John Otander <johnotander@gmail.com>
2015-10-30 12:41:20 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,26 @@
+import test from 'ava'
+import singleTrailingNewline from './'
+
+const strings = [
+  'foo\nbar',
+  'foo\nbar\n',
+  'foo\nbar\n\n',
+  'foo\nbar\n\r\n',
+  'foo\nbar\n\n\n\r\n\r\n'
+]
+
+const carriageReturnStrings = [
+  'foo\r\nbar',
+  'foo\r\nbar\r\n',
+  'foo\r\nbar\r\n\r\n',
+  'foo\r\nbar\n\r\n',
+  'foo\nbar\r\n\r\n'
+]
+
+test('single-trailing-newline detects \\n strings and ensures only one trailing newline', t => {
+  t.plan(strings.length)
+
+  strings.forEach(str => {
+    t.same(singleTrailingNewline(str), 'foo\nbar\n')
+  })
+})
